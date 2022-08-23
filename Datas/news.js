089f9b3b11445a7acff384f20da25fc93bdb8e86
y2Dev <dub-y@tuta.io>
2022-08-23 10:09:40 -0400

    Mise en place d'un fichier de données pour les news

---
+++ Datas/news.js
@@ -0,0 +1,44 @@
+export const news = [
+    {
+        id : 1,
+        image : require("../AllApps/AppNews/images/business-school-alumni.png"),
+        titre : "Business School Alumni",
+        description : "Pour organiser un session de brainstorming en école de commerce, ..."
+    },
+    {
+        id : 2,
+        image : require("../AllApps/AppNews/images/maxresdefault.jpg"),
+        titre : "Développer ressources insoupsonnées",
+        description : "Dans une situation d'épreuve, nous pouvons mobiliser un certains nombre de ressources..."
+    },
+    {
+        id : 3,
+        image : require("../AllApps/AppNews/images/maxresdefault2.jpg"),
+        titre : "Votre profil piston",
+        description : "Voici une façon d'améliorer l'impact de votre profil social"
+    },
+    {
+        id : 4,
+        image : require("../AllApps/AppNews/images/maxresdefault3.jpg"),
+        titre : "Accroître l'agilité de vos processus",
+        description : "Si nous avons besoin d'adapter notre flux de travail, ..."
+    },
+    {
+        id : 5,
+        image : require("../AllApps/AppNews/images/maxresdefault4.jpg"),
+        titre : "Développer votre leadership",
+        description : "Nous invitons Gisèle Szczyglak pour nous parler de son dernier livre..."
+    },
+    {
+        id : 6,
+        image : require("../AllApps/AppNews/images//PlayStation-Emblema.png"),
+        titre : "PlayStation fait face à une poursuite de 5 milliards d'euros pour avoir prétendument « escroqué » des clients",
+        description : "Les joueurs au UK ont été « surfacturés » pour leurs achats numériques selon la plainte"
+    },
+    {
+        id : 7,
+        image : require("../AllApps/AppNews/images/Oracle-Simbolo.png"),
+        titre : "Oracle fait l'objet d'une action collective pour avoir pisté cinq milliards de personnes",
+        description : "L'action en justice à l'intention d'Oracle vise à arrêter sa machine de surveillance mondiale"
+    },
+] ;
